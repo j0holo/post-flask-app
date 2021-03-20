0744b06be93bae9f6e697ae1fecd78f72d8d524c
Matheus <matheusphranco@gmail.com>
2021-03-20 14:58:38 -0300

    Create basic create_app setup

--- app/app.py
+++ app/app.py
@@ -1,5 +1,29 @@
+from .models import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 from flask import Flask
-app = Flask(__name__)
 
 
-def create
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
