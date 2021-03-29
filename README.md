ac3e233f6fd3389c31e0213c6a96a153f5771e32
Ruben Klink <10252030+j0holo@users.noreply.github.com>
2021-03-29 21:30:14 +0200

    Feat/project setup (#1)
    * Create migrations using flask-migrate
    
    * Add docs to how to setup some parts of the project
    
    * Ignore .env
    
    * Add file to hold the configs
    
    * Add the dependencies
    
    * Create User model
    
    * Create basic create_app setup
    
    * Add the first two tests
    
    It is now possible to test components without
    spinning up a complete Flask server.
    
    * Remove tq file that got created
    
    * Add newline to README.md
    
    Co-authored-by: Matheus <matheusphranco@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+# How generate a secret key
+
+```bash
+python -c "import secrets;print(secrets.token_urlsafe(50))"
+```
+
+# .env sample
+
+```bash
+SECRET_KEY=secret_key
+```
+
+# Manually installed dependencies
+
+* <b>flask</b>: the microframework web
+* <b>python-dotenv</b>: python package to load .env values to environment variables
+
+# Run unit tests
+
+1. `docker run --rm -d -p 5432:5432 --name=postgres-flask -e POSTGRES_PASSWORD=password -e POSTGRES_DB=flask  postgres:13`
+1. `pytest`
--- app/app.py
+++ app/app.py
@@ -1,5 +1,25 @@
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 from flask import Flask
-app = Flask(__name__)
 
 
-def create
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+    )
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
