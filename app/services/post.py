5a56fd0c7a098e8d50f1445b06f1af294ce4e1b5
Matheus <matheusphranco@gmail.com>
2021-05-01 18:26:36 -0300

    Add create and update function to post service

--- app/services/post.py
+++ app/services/post.py
@@ -4,18 +4,38 @@ from app.exceptions import PostNotFoundError
 from app.postgres.post import Post
 
 
-def get_post_by_slug(connection: connection, slug: str, author: int):
-    with connection.cursor(cursor_factory=DictCursor) as curs:
-        query = """\
+def generate_slug(title: str) -> str:
+    return title.replace(" ", "-").lower()
+
+
+def create_post(conn: connection, author: int, title: str, content: str):
+    with conn.cursor() as cursor:
+        query = """
+            INSERT INTO posts (title, slug, author, content)
+            VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(query, (title, generate_slug(title), author, content))
+    
+
+def update_post(conn: connection, post_id: int, title: str, content: str):
+    with conn.cursor() as cursor:
+        query = "UPDATE posts SET title=%s, content=%s WHERE id=%s"
+        cursor.execute(query, (title, content, post_id))
+
+
+def get_post_by_slug(conn: connection, slug: str, author: int):
+    with conn.cursor(cursor_factory=DictCursor) as curs:
+        query = """
             SELECT id, title, slug, author, posted_at, updated_at, content\
-            FROM posts\
-            WHERE slug=%s AND author=%s\
+            FROM posts
+            WHERE slug=%s AND author=%s
         """
         curs.execute(query, (slug, author))
         row = curs.fetchone()
 
     if row is None:
         raise PostNotFoundError
+
     return Post(
         row['id'], 
         row['title'], 
--- test/test_service_post.py
+++ test/test_service_post.py
@@ -1,5 +1,10 @@
 import pytest
-from app.services.post import get_post_by_slug
+from app.services.post import (
+    get_post_by_slug, 
+    create_post, 
+    update_post
+)
+from app.services.user import create
 from psycopg2 import DatabaseError, DataError
 
 # /<username>/posts/<slug>/
@@ -12,17 +17,15 @@ def test_get_post_by_slug(conn):
     author = 1
     content = "BLA BLA BLA"
 
-    cursor = conn.cursor()
     try:
-        cursor.execute("INSERT INTO users (username) VALUES (%s)", (username, ))
-        conn.commit()
-        query = """\
-            INSERT INTO posts (title, slug, author, content)\
-            VALUES (%s, %s, %s, %s)\
-        """
-        cursor.execute(query, (title, slug, author, content))
-        conn.commit()
-        cursor.close()
+        with conn.cursor() as cursor:
+            cursor.execute("INSERT INTO users (username) VALUES (%s)", (username, ))
+            conn.commit()
+            query = """\
+                INSERT INTO posts (title, slug, author, content)\
+                VALUES (%s, %s, %s, %s)\
+            """
+            cursor.execute(query, (title, slug, author, content))
     except (DataError, DatabaseError) as e:
         pytest.fail(e)
         
@@ -31,3 +34,37 @@ def test_get_post_by_slug(conn):
     assert post.author == author
     assert post.content == content
     assert post.title == title
+
+
+def test_create_post(conn):
+    author = 1
+    title = "Pytest test"
+    slug = "pytest-test"
+    content = "foo"
+    create(conn, "username", "username@gmail.com", "123456", "123456", "profile")
+    create_post(conn, author, title, content)
+    post = get_post_by_slug(conn, slug, author)
+    assert post.title == title
+    assert post.slug == slug
+    assert post.author == author
+    assert post.content == content
+
+def test_update_post(conn):
+    author = 1
+    old_title = "Pytest test"
+    old_slug = "pytest-test"
+    old_content = "foo"
+    new_title = "Pytest test 2"
+    new_slug = "pytest-test-2"
+    new_content = "bar"
+
+    create(conn, "username", "username@gmail.com", "123456", "123456", "profile")
+    create_post(conn, author, old_title, old_content)
+    old_post = get_post_by_slug(conn, old_slug, author)
+    
+    create_post(conn, author, new_title, new_content)
+    new_post = get_post_by_slug(conn, new_slug, author)
+    
+    assert not new_post.content == old_post.content
+    assert not new_post.title == old_post.title
+    assert not new_post.content == old_post.content
