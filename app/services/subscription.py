df81c1b94270ab4db0850c4682c3479290b81cf6
Ruben Klink <10252030+j0holo@users.noreply.github.com>
2021-05-29 21:01:01 +0200

    Feat/subscribe to author (#11)
    * Add subscription database functions
    
    Also removed unused imports in test module.
    
    * Rename get_authors to get_authors_and_posts and add get_authors func
    
    * Add subscription service
    
    Co-authored-by: Ruben Klink <ruben@mrchadd.nl>

---
+++ app/services/subscription.py
@@ -0,0 +1,16 @@
+from typing import List
+from app.postgres import subscription
+from typing import List
+
+def subscribe(conn, user_id, author_id):
+    subscription.subscribe(conn, user_id, author_id)
+
+def unsibscribe(conn, user_id, author_id):
+    subscription.unsubscribe(conn, user_id, author_id)
+
+def get_authors_and_posts(conn, user_id) -> List[subscription.Subscription]:
+    return subscription.get_authors_and_posts(conn, user_id)
+
+def get_authors_and_posts(conn, user_id) -> List[str]:
+    return subscription.get_authors_and_posts(conn, user_id)
+
---
+++ test/test_postgres_subscription.py
@@ -0,0 +1,117 @@
+import pytest
+from app.postgres import subscription, user
+from app.services import post as post_service
+from psycopg2 import DatabaseError, DataError
+
+
+def test_subscribe_to_author(conn):
+    email1 = "john@example.com"
+    email2 = "karen@example.com"
+
+    try:
+        user.create(conn, email1, email1, "password", "")
+        user.create(conn, email2, email2, "password", "")
+
+        user1 = user.get_by_email(conn, email1)
+        user2 = user.get_by_email(conn, email2)
+
+        conn.commit()
+    except DataError as e:
+        pytest.fail(e)
+    except DatabaseError as e:
+        pytest.fail(e)
+
+    subscription.subscribe(conn, user1.id, user2.id)
+
+def test_unsubscribe(conn):
+    email1 = "john@example.com"
+    email2 = "karen@example.com"
+
+    try:
+        user.create(conn, email1, email1, "password", "")
+        user.create(conn, email2, email2, "password", "")
+
+        user1 = user.get_by_email(conn, email1)
+        user2 = user.get_by_email(conn, email2)
+        subscription.subscribe(conn, user1.id, user2.id)
+        conn.commit()
+    except DataError as e:
+        pytest.fail(e)
+    except DatabaseError as e:
+        pytest.fail(e)
+
+    subscription.unsubscribe(conn, user1.id, user2.id)
+    assert len(subscription.get_authors(conn, user1.id)) == 0
+
+def test_get_authors_and_posts(conn):
+    user1 = user.User(0, "user1", "john@example.com", "")
+    user2 = user.User(0, "user2", "karen@example.com", "")
+
+    try:
+        user.create(conn, user1.username, user1.email, "password", "")
+        user.create(conn, user2.username, user2.email, "password", "")
+
+        user1 = user.get_by_email(conn, user1.email)
+        user2 = user.get_by_email(conn, user2.email)
+
+        post_service.create(conn, user2.id, "first post", "content")
+        post_service.create(conn, user2.id, "second post", "content")
+
+        subscription.subscribe(conn, user1.id, user2.id)
+        conn.commit()
+
+        subscriptions = subscription.get_authors_and_posts(conn, user1.id)
+    except DataError as e:
+        pytest.fail(e)
+    except DatabaseError as e:
+        pytest.fail(e)
+
+    for sub in subscriptions:
+        assert sub.author == user2.username
+        for p in sub.posts:
+            assert p.author == user2.username
+            assert p.title in ["first post", "second post"]
+            assert p.content == "content"
+
+def test_get_authors(conn):
+    user1 = user.User(0, "user1", "john@example.com", "")
+    user2 = user.User(0, "user2", "karen@example.com", "")
+    user3 = user.User(0, "user3", "dan@example.com", "")
+
+    try:
+        user.create(conn, user1.username, user1.email, "password", "")
+        user.create(conn, user2.username, user2.email, "password", "")
+        user.create(conn, user3.username, user3.email, "password", "")
+
+        user1 = user.get_by_email(conn, user1.email)
+        user2 = user.get_by_email(conn, user2.email)
+        user3 = user.get_by_email(conn, user3.email)
+
+        subscription.subscribe(conn, user1.id, user2.id)
+        subscription.subscribe(conn, user1.id, user3.id)
+        conn.commit()
+
+        authors = subscription.get_authors(conn, user1.id)
+    except DataError as e:
+        pytest.fail(e)
+    except DatabaseError as e:
+        pytest.fail(e)
+
+    assert "user2" in authors
+    assert "user3" in authors
+    assert "user1" not in authors
+
+def test_get_authors_without_subscriptions(conn):
+    user1 = user.User(0, "user1", "john@example.com", "")
+
+    try:
+        user.create(conn, user1.username, user1.email, "password", "")
+        conn.commit()
+
+        authors = subscription.get_authors(conn, user1.id)
+    except DataError as e:
+        pytest.fail(e)
+    except DatabaseError as e:
+        pytest.fail(e)
+
+    assert len(authors) == 0
